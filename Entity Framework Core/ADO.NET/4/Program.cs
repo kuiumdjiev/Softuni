2fa852b7470530c255e44b44ed718acba239a3ca
Stefan Kuiumdjiev <75949309+CortCart@users.noreply.github.com>
2021-10-17 13:24:24 +0300

    Add files via upload

---
+++ Entity Framework Core/ADO.NET/4/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using  System.Data.SqlClient;
+using System.Diagnostics;
+using System.Linq;
+
+
+namespace _4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] info = Console.ReadLine().Split(' ').ToArray();
+            string name = info[1];
+            int age = int.Parse(info[2]);
+            string town = info[3];
+
+            string villan = Console.ReadLine().Split(' ').ToArray()[1];
+
+            //connect to minions
+            using SqlConnection connection = new SqlConnection("Server=.;Database=MinionsDB;Integrated Security=true");
+            connection.Open();
+
+            string townCheck = @"SELECT Id FROM Towns WHERE @Name = Name";
+            string minionCheck = @"SELECT Id FROM Minions WHERE @Name = Name";
+            string villainCheck = @"SELECT Id FROM Villains WHERE @Name = Name";
+            //town
+            using SqlCommand commandTown = new SqlCommand(townCheck, connection);
+            commandTown.Parameters.AddWithValue("@Name",town);
+            object result = commandTown.ExecuteScalar();
+            if (result is null)
+            {
+                string addTown = @"INSERT INTO Towns (Name) VALUES (@townName)";
+                using SqlCommand commandAddTown = new SqlCommand(addTown, connection);
+                commandAddTown.Parameters.AddWithValue("@townName", town);
+                commandAddTown.ExecuteNonQuery();
+                Console.WriteLine($"Town {town} was added to the database.");
+            }
+            //villain
+            using SqlCommand commandVillain = new SqlCommand(villainCheck, connection);
+            commandVillain.Parameters.AddWithValue("@Name", villainCheck);
+            object resultVillain = commandVillain.ExecuteScalar();
+            if (resultVillain is null)
+            {
+                string addVillain = @"INSERT INTO Villains (Name, EvilnessFactorId) VALUES (@Name, 4)";
+                using SqlCommand commandAddVillain = new SqlCommand(addVillain, connection);
+                commandAddVillain.Parameters.AddWithValue("@Name", villan);
+                commandVillain.ExecuteNonQuery();
+                Console.WriteLine($"Villain {villan} was added to the database.");
+            }
+            //minion
+            using SqlCommand commandMinions = new SqlCommand(minionCheck, connection);
+            commandMinions.Parameters.AddWithValue("@Name", name);
+            object resultMinion = commandMinions.ExecuteScalar();
+            if (resultMinion is null)
+            {
+             
+                using SqlCommand commandTownId = new SqlCommand(townCheck, connection);
+                commandTownId.Parameters.AddWithValue("@TownName", town);
+                int townId = (int)commandTownId.ExecuteScalar();
+
+                string addMinion = @"INSERT INTO Minions (Name, Age, TownId) VALUES (@Name, @Age, @TownId)";
+                using SqlCommand commandAddMinion = new SqlCommand(addMinion, connection);
+                commandAddMinion.Parameters.AddWithValue("@Name", name);
+                commandAddMinion.Parameters.AddWithValue("@Age", age);
+                commandAddMinion.Parameters.AddWithValue("@TownId", townId);
+                commandAddMinion.ExecuteNonQuery();
+            }
+            else
+            {
+                
+            }
+        }
+    }
+}
---
+++ Entity Framework Core/ADO.NET/5/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using  System.Data.SqlClient;
+
+namespace _5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+
+            //connect to minions
+            using SqlConnection connection = new SqlConnection("Server=.;Database=MinionsDB;Integrated Security=true");
+            connection.Open();
+
+            string update = @"UPDATE Towns 
+                            SET [Name] = UPPER([Name])
+                            WHERE CountryCode = (SELECT ID FROM Countries
+					                            WHERE [Name] = @name)";
+
+            using SqlCommand commandUpper = new SqlCommand(update, connection);
+            commandUpper.Parameters.AddWithValue("@name", name);
+
+            int count = commandUpper.ExecuteNonQuery();
+
+            if (count ==0 )
+            {
+                Console.WriteLine("No town names were affected.");
+            }
+            else
+            {
+                Console.WriteLine($"{count} town names were affected.");
+                string getName = @"SELECT [Name]  FROM Towns
+                                    WHERE CountryCode = (SELECT ID FROM Countries
+					                WHERE [Name] = @name)";
+
+                using SqlCommand commandGetNames = new SqlCommand(getName, connection);
+                using SqlDataReader info =    commandGetNames.ExecuteReader();
+                List<string> towns = new List<string>();
+
+                while (info.Read())
+                {
+                    towns.Add(info["Name"].ToString());
+                }
+
+                Console.WriteLine(string.Join(", ", towns));
+
+            }
+        }
+    }
+}
