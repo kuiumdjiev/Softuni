ad505da7545ac341549c46b6aef0f6b4e07f5f25
Stefan Kuiumdjiev <75949309+CortCart@users.noreply.github.com>
2021-10-23 20:24:21 +0300

    Add files via upload

---
+++ Entity Framework Core/ADO.NET/2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace _2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //connect to minions
+            using SqlConnection connection = new SqlConnection("Server=.;Database=MinionsDB;Integrated Security=true");
+            connection.Open();
+
+            string query = @"SELECT v.Name + ' - ' + CONVERT(NVARCHAR,COUNT(mv.MinionId)) AS Output FROM Villains AS v 
+                           JOIN MinionsVillains AS mv ON mv.VillainId = v.Id
+                           GROUP BY v.Id, v.Name
+                           HAVING COUNT(mv.MinionId) > 2
+                           ORDER BY COUNT(mv.MinionId)";
+
+            using SqlCommand command = new SqlCommand(query, connection);
+            using SqlDataReader reader =command.ExecuteReader();
+
+            while (reader.Read())
+            {
+                Console.WriteLine($"{reader["Output"]}");                
+            }
+
+
+        }
+    }
+}
