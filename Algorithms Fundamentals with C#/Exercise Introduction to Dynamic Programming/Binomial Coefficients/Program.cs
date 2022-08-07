d02a2e6c5512f7dce42ea0442c10f9aa2233ac57
Stefan Kuiumdjiev <stefan.kuiumdjiev@gmail.com>
2022-08-07 13:00:00 +0300

    Added Introduction to Dynamic Programming

---
+++ Algorithms Fundamentals with C#/Exercise Introduction to Dynamic Programming/Binomial Coefficients/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+
+namespace Binomial_Coefficients
+{
+    public class Program
+    {
+        private static Dictionary<string, long> dp;
+        static void Main()
+        {
+            dp = new Dictionary<string, long>();
+
+            int row = int.Parse(Console.ReadLine());
+            int col = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(BinomialCoefficients(row,col));
+        }
+
+        private static long BinomialCoefficients(int row, int col)
+        {
+            if (col==0||row==col)
+            {
+                return 1;
+            }
+
+            var key = $"{row}-{col}";
+
+            if (dp.ContainsKey(key))
+            {
+                return dp[key];
+            }
+            var  result= BinomialCoefficients(row - 1, col - 1) + BinomialCoefficients(row - 1, col);
+            dp[key]=result;
+            return result;
+        }
+    }
+}
