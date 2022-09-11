89ee2653337ec232fee680f6fb773609854088c7
Stefan Kuiumdjiev <stefan.kuiumdjiev@gmail.com>
2022-09-11 14:30:26 +0300

    Added Graphs Bellman-Ford, Longest Path in (DAG)

---
+++ Algorithms Advanced  with C#/Graphs Bellman-Ford, Longest Path in (DAG)/Bellman-Ford/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Bellman_Ford
+{
+    public class Edge
+    {
+        public int From { get; set; }
+
+        public int To { get; set; }
+
+        public int Weight { get; set; }
+    }
+
+    public class Program
+    {
+        static void Main()
+        {
+            var nodes = int.Parse(Console.ReadLine());
+            var edges = int.Parse(Console.ReadLine());
+
+            var graph = new List<Edge>();
+
+            for (int i = 0; i < edges; i++)
+            {
+                var edgeData = Console.ReadLine()
+                    .Split(" ")
+                    .Select(int.Parse)
+                    .ToArray();
+
+                graph.Add(new  Edge
+                {
+                    From = edgeData[0],
+                    To = edgeData[1],
+                    Weight = edgeData[2]
+                });
+            }
+
+            var source = int.Parse(Console.ReadLine());
+            var destination =int.Parse( Console.ReadLine());
+
+            var distance = new double[nodes+1];
+            for (int i = 0; i < distance.Length; i++)
+            {
+                distance[i] = double.PositiveInfinity;
+            }
+            distance[source] = 0;
+
+            var prev = new int[nodes +1];
+            for (int i = 0; i < distance.Length; i++)
+            {
+                distance[i] =-1;
+            }
+
+            for (int i = 0; i < nodes-1; i++)
+            {
+                var updated = false;
+                foreach (var edge in graph)
+                {
+                    if (double.IsPositiveInfinity(distance[edge.From]))
+                    {
+                        continue;
+                    }
+
+                    var newDistance = distance[edge.From] + edge.Weight;
+                    if (newDistance< distance[edge.To])
+                    {
+                        distance[edge.To] = newDistance;
+                        prev[edge.To] = edge.From;
+                        updated = true;
+                    }
+                }
+
+                if (updated)
+                {
+                    break;
+                }
+            }
+
+            foreach (var edge in graph)
+            {
+                var newDistance = distance[edge.From] + edge.Weight;
+                if (newDistance < distance[edge.To])
+                {
+                    Console.WriteLine("Negative Cycle Detected");
+                    return;
+                }
+            }
+
+            var path = new Stack<int>();
+            var node = destination;
+
+            while (node!=-1)
+            {
+                path.Push(node);
+                node = prev[node];
+            }
+
+            Console.WriteLine(string.Join(" ", path));
+
+            Console.WriteLine(distance[destination]);
+        }
+    }
+}
---
+++ Algorithms Advanced  with C#/Graphs Bellman-Ford, Longest Path in (DAG)/Longest Path/Program.cs
@@ -0,0 +1,112 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Longest_Path
+{
+    public class Edge
+    {
+        public int From { get; set; }
+
+        public int To { get; set; }
+
+        public int Weight { get; set; }
+    }
+
+    public class Program
+    {
+        private static Dictionary<int, List<Edge>> edgesByNode;
+
+        static void Main()
+        {
+            var nodes = int.Parse(Console.ReadLine());
+            var edges = int.Parse(Console.ReadLine());
+
+            edgesByNode = new Dictionary<int, List<Edge>>();
+
+            for (int i = 0; i < edges; i++)
+            {
+                var edgeData = Console.ReadLine()
+                    .Split(" ")
+                    .Select(int.Parse)
+                    .ToArray();
+
+                var from = edgeData[0];
+                var to = edgeData[1];
+
+                if (!edgesByNode.ContainsKey(from))
+                {
+                    edgesByNode.Add(from, new List<Edge>());
+                }
+
+                if (!edgesByNode.ContainsKey(to))
+                {
+                    edgesByNode.Add(to , new List<Edge>());
+                }
+
+                edgesByNode[from].Add(new Edge
+                {
+                    From = from,
+                    To = to,
+                    Weight = edgeData[2]
+                });
+            }
+
+            var source = int.Parse(Console.ReadLine());
+            var destination = int.Parse(Console.ReadLine());
+
+            var distance = new double[nodes + 1];
+            Array.Fill(distance, double.NegativeInfinity);
+
+            distance[source] = 0;
+
+            var sorted = TopicalSorting();
+
+            while (sorted.Count>0)
+            {
+                var node = sorted.Pop();
+                foreach (var edge in edgesByNode[node])
+                {
+                    var newDistance = distance[edge.From] + edge.Weight;
+
+                    if (newDistance> distance[edge.To])
+                    {
+                        distance[edge.To] = newDistance;
+                    }
+                }
+            }
+
+            Console.WriteLine(distance[destination]);
+        }
+
+        private static Stack<int> TopicalSorting()
+        {
+            var result = new Stack<int>();
+            var  visited= new HashSet<int>();
+
+            foreach (var node in edgesByNode.Keys)
+            {
+                DFS(node, visited, result);
+            }
+            return result ;
+        }
+
+        private static void DFS(int node, HashSet<int> visited, Stack<int> result)
+        {
+            if (visited.Contains(node))
+            {
+                return;
+            }
+
+            visited.Add(node);
+
+            foreach (var edge in edgesByNode[node])
+            {
+                DFS(edge.To,
+                     visited, result);
+            }
+            result.Push(node);
+        }
+    }
+}
