feb1132f31e1478e821d4e094ac9942fc1714db6
Stefan Kuiumdjiev <stefan.kuiumdjiev@gmail.com>
2022-09-18 14:48:54 +0300

    Added Graphs Strongly Connected Components, Max Flow

---
+++ Algorithms Advanced  with C#/Graphs Strongly Connected Components, Max Flow/Max Flow algorithm - Edmonds-Karp/Program.cs
@@ -0,0 +1,100 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace Max_Flow_algorithm___Edmonds_Karp
+{
+    public class Program
+    {
+        private static int[,] graph;
+        private static int[] parent;
+       public static void Main()
+        {
+            var nodes = int.Parse(Console.ReadLine());
+             graph = new int[nodes, nodes];
+             parent = new int[nodes];
+             Array.Fill(parent,-1);
+
+            for (int i = 0; i < nodes; i++)
+            {
+                var rows = Console.ReadLine()
+                    .Split(", ")
+                    .Select(int.Parse)
+                    .ToArray();
+                for (int j = 0; j < nodes; j++)
+                {
+                    graph[j,j] = rows[i];
+                }
+            }
+
+            var source = int.Parse(Console.ReadLine());
+            var target = int.Parse(Console.ReadLine());
+            var maxFlow = 0;
+
+            while (BFS(source,target))
+            {
+                var minFlow = GetMinFlow(target);
+                ApplyFlow(target, minFlow);
+                maxFlow += minFlow;
+            }
+
+            Console.WriteLine($"Max flow = {maxFlow}");
+        }
+
+        private static void ApplyFlow(int node, int minFlow)
+        {
+            while (parent[node] != -1)
+            {
+                var prev = parent[node];
+                 graph[prev, node]-=minFlow;
+                node = prev;
+            }
+        }
+
+        private static int GetMinFlow(int node)
+        {
+            int minFlow = int.MaxValue;
+            while (parent[node]!=-1)
+            {
+                var prev = parent[node];
+                var flow = graph[prev, node];
+                if (flow < minFlow)
+                {
+                    minFlow = flow;
+                }
+                node = prev;
+            }
+
+            return minFlow;
+        }
+
+        private static bool BFS( int source, int target)
+        {
+            var visited = new bool[graph.GetLength(0)];
+            visited[source] = true;
+
+            var queue = new Queue<int>();
+            queue.Enqueue(source);
+
+            while (queue.Count>0)
+            {
+                var node = queue.Dequeue();
+
+                for (int child = 0; child < graph.GetLength(1); child++)
+                {
+                    if (!visited[child]&& graph[node,child]>0)
+                    {
+                        visited[child] = true;
+                        queue.Enqueue(child);
+                        parent[child] = node;
+                    }
+                }
+            }
+
+            return visited[target];
+        }
+    }
+}
---
+++ Algorithms Advanced  with C#/Graphs Strongly Connected Components, Max Flow/Strongly Connected Components (SCC)/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+
+namespace Strongly_Connected_Components__SCC_
+{
+    public class Program
+    {
+        static void Main()
+        {
+            var nodes = int.Parse(Console.ReadLine());
+            var lines = int.Parse(Console.ReadLine());
+
+            var graph = new List<int>[nodes];
+            var revarseGraph = new List<int>[nodes];
+
+
+            for (int i = 0; i < graph.Length; i++)
+            {
+                graph[i] = new List<int>();
+                revarseGraph[i] = new List<int>();
+            }
+
+            for (int i = 0; i < lines; i++)
+            {
+                var line = Console.ReadLine()
+                    .Split(", ")
+                    .Select(int.Parse)
+                    .ToArray();
+
+                var node = line[0];
+
+                for (int j = 1; j < line.Length; j++)
+                {
+                    var child = line[j];
+
+                    graph[node].Add(child);
+                    revarseGraph[child].Add(node);
+
+                }
+
+            }
+
+            var visited = new bool[graph.Length];
+            var sorted = new Stack<int>();
+
+
+            for (int node = 0; node < graph.Length; node++)
+            {
+                DFS(graph, node, visited, sorted);
+            }
+
+          visited = new bool[graph.Length];
+          Console.WriteLine("Strongly Connected Components:");
+            while (sorted.Count > 0)
+            {
+                var node = sorted.Pop();
+                var commponent = new Stack<int>();
+                if (visited[node])
+                {
+                    continue;
+                }
+
+                DFS(revarseGraph, node, visited, commponent);
+                Console.Write("{");
+                Console.Write(string.Join(", ", commponent));
+                Console.WriteLine("}");
+            }
+        }
+
+     private   static void DFS(List<int>[] graph, int node, bool[] visited, Stack<int> sorted)
+            {
+                if (visited[node])
+                {
+                    return;
+                }
+
+                visited[node] = true;
+                foreach (var child in graph[node])
+                {
+                    DFS(graph, child, visited, sorted);
+                }
+
+                sorted.Push(node);
+            }
+        }
+    }
+
