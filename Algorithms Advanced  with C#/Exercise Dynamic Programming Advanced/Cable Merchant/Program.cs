b2aed01e126c0abc61f7cca06a0c2911f73b0f47
Stefan Kuiumdjiev <stefan.kuiumdjiev@gmail.com>
2022-09-21 19:56:55 +0300

    Added Exercise: Dynamic Programming Advanced

---
+++ Algorithms Advanced  with C#/Exercise Dynamic Programming Advanced/Cable Merchant/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace Rod_Cutting​
+{
+    public class Program
+    {
+
+        private static List<int> price;
+        private static int[] bestPrices;
+
+        static void Main()
+        {
+            price= new List<int>{0}; 
+                price.AddRange( Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                );
+
+            bestPrices = new int[price.Count];
+
+            int connectorPrice = int.Parse(Console.ReadLine());
+
+CutRod(price.Count-1 , connectorPrice);
+
+Console.WriteLine(string.Join(" ",bestPrices.Skip(1)));
+
+        }
+
+        public static int CutRod(int length, int connectorPrice)
+        {
+
+            if (length == 0)
+            {
+                return 0;
+            }
+
+            if (bestPrices[length] != 0)
+            {
+                return bestPrices[length];
+            }
+
+            var bestPrice = price[length];
+
+
+            for (int i = 1; i < length; i++)
+            {
+
+                var price = Program.price[i] + CutRod(length - i, connectorPrice) -2*connectorPrice;
+
+                if (price > bestPrice)
+                {
+                    bestPrice = price;
+                }
+            }
+
+            bestPrices[length] = bestPrice;
+
+
+            return bestPrice;
+
+        }
+    }
+}
---
+++ Algorithms Advanced  with C#/Exercise Dynamic Programming Advanced/Longest String Chain/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Longest_String_Chain
+{
+    public class Program
+    {
+        public static string[] strings;
+        public static int[] lengths;
+        public static int[] prev;
+        static void Main()
+        {
+            strings = Console.ReadLine().Split().ToArray();
+            lengths = new int[strings.Length];
+            prev = new int[strings.Length];
+            prev[0] = -1;
+            lengths[0] = 1;
+            int indexOfMaximumLength = -1, maxLen = 0;
+            for (int i = 0; i < strings.Length; i++)
+            {
+                var bestLength = 1;
+                var prevIndex = -1;
+                var currentString = strings[i];
+                for (int j = i - 1; j >= 0; j--)
+                {
+                    var previousString = strings[j];
+                    if (previousString.Length < currentString.Length && bestLength <= lengths[j] + 1)
+                    {
+                        bestLength = lengths[j] + 1;
+                        prevIndex = j;
+                    }
+                }
+
+                lengths[i] = bestLength;
+                prev[i] = prevIndex;
+                if (bestLength > maxLen)
+                {
+                    maxLen = bestLength;
+                    indexOfMaximumLength = i;
+                }
+            }
+            List<string> chain = new List<string>();
+            while (indexOfMaximumLength != -1)
+            {
+                chain.Add(strings[indexOfMaximumLength]);
+                indexOfMaximumLength = prev[indexOfMaximumLength];
+            }
+            chain.Reverse();
+            Console.WriteLine(string.Join(" ", chain));
+        }
+    }
+}
